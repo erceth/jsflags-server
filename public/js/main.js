e3ec8d42fe4db2ee03865c2a2235353fd96d8025
Eric <erceth@gmail.com>
2015-11-22 19:43:11 -0600

    switched to angular

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,32 @@
-var socket = io();
+angular.module("app", []).config(config).controller("AppCtrl", AppCtrl);
 
-socket.on('options', function(options){ 
-	var gameElement = $('#games');
-	gameElement.empty();
+function config() {
 
-	//create the active game buttons, if any
-	for (var i = 0; i < options.games.length; i++) {
-		var buttonElement = $('<div class="button active"></div>');
-		buttonElement.data(buttonElement, 'id', options.games[i].id);
-		buttonElement.append('<a target="_blank" href=":' + options.games[i].port + '">Join game</a>');
-		gameElement.append(buttonElement);
-		//TODO show number of players playing (e.g. 3 out of 4)
-	}
-	//create available game buttons, if any
-	var availableGames = options.numberOfAllowedGames - options.games.length;
-	for (var j = 0; j < availableGames; j++) {
-		var buttonElement2 = $('<div class="button available">Create Game</div>');
-		gameElement.append(buttonElement2);
-		buttonElement2.on('click', createGame);
-	}
-
-});
-
-/**
- * function to run when join game button is clicked
- */
-function createGame (e) {
-	socket.emit('createGame');
-	console.log(e);
 }
 
+function AppCtrl($scope) {
+    var socket = io();
+    socket.on('options', function(options) {
+        $scope.activeGames = options.games;
+        var availableGames = options.numberOfAllowedGames - options.games.length;
+        $scope.availableGames = new Array(availableGames);
+        updateScope();
+    });
+
+
+    /**
+     * function to run when join game button is clicked
+     */
+    $scope.createGame = function() {
+        socket.emit('createGame');
+    }
+
+    function updateScope() {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    }
+
+
+}
 
